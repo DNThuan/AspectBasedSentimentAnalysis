4be09e2f4d3dd5300ab8376b867ae36470bec4ff
DNThuan <67100446+DNThuanUIT@users.noreply.github.com>
2021-12-02 09:29:45 +0700

    update utils

---
+++ utils/preprocess_label.py
@@ -0,0 +1,155 @@
+import numpy as np
+import pandas as pd
+import json
+from sklearn.preprocessing import MultiLabelBinarizer
+
+aspect_path = "./Label/aspect.json"
+SA_path = "./Label/SA.json"
+
+
+def read_label(path):
+  with open(path) as f:
+    data = json.load(f)
+  f.close()
+  return data
+
+aspect_labels = read_label(aspect_path)
+AS_labels = read_label(SA_path)
+
+
+
+def find_start_end(label):
+  start = 0
+  end = 0
+  lst_start=[]
+  lst_end=[]
+  for index ,char in enumerate(label):
+    if char == "{":
+      start = index
+      lst_start.append(start)
+    elif char == "}":
+      end = index
+      lst_end.append(end)
+  return tuple(zip(lst_start,lst_end))
+
+
+def Label_to_ListDict(labels):
+  list_dict_label = list()
+  for label in labels:
+    lst = []
+    index = tuple(find_start_end(label))
+    for i in index:
+      dict_label = dict()
+      aspect, polarity = label[i[0]+1:i[1]].replace(" ","").split(",")
+      dict_label[aspect] = polarity
+      lst.append(dict_label)
+    list_dict_label.append(lst)
+  return list_dict_label
+
+
+def Label_str_to_list(label):
+  index = tuple(find_start_end(label))
+  aspect_temp=[]
+  polarity_temp=[]
+  for i in index:
+    temp = label[i[0]+1:i[1]].replace(" ","").split(",")
+    aspect_temp.append(temp[0])
+    polarity_temp.append(temp[1])
+  return aspect_temp, polarity_temp
+
+def separate_label(labels):
+  aspect= []
+  polarity = []
+  SA = []
+  for label in labels:
+    temp = Label_str_to_list(label)
+    aspect.append(temp[0])
+    polarity.append(temp[1])
+
+    sa_temp= []
+    for i in range(len(temp[0])):
+      sa = "{"+temp[0][i]+", "+temp[1][i]+"}"
+      sa_temp.append(sa)
+    SA.append(sa_temp)
+
+  return np.array(aspect, dtype=object), np.array(polarity, dtype=object), np.array(SA, dtype=object)
+
+def show_label(list_label, pred_as, pred_pos, pred_neg, pred_neu):
+  labels = []
+  for index, value in enumerate(pred_as):
+    if value == 1:
+      label = "{"
+      if pred_pos[index] ==1:
+        label += str(list_label[index])+", "+"positive"
+      elif pred_neg[index] ==1:
+        label += str(list_label[index])+", "+"negative"
+      elif pred_neu[index] ==1:
+        label += str(list_label[index])+", "+"neutral"
+      label+="}"
+      labels.append(label)
+  return labels
+
+
+
+def Binary_label():
+
+
+    transform_label_aspect = MultiLabelBinarizer().fit([aspect_labels])
+    list_label_aspect = transform_label_aspect.classes_
+    transform_label_SA = MultiLabelBinarizer().fit([AS_labels])
+
+    return transform_label_aspect, transform_label_SA, list_label_aspect
+
+      
+def create_label_dataframe(labels):
+    aspect, polarity,_ = separate_label(labels)
+    bi_label_aspect, _, _ = Binary_label()
+    aspect_tf = bi_label_aspect.transform(aspect)
+    for index1,label in enumerate(aspect_tf):
+        count = 0
+        for index2,a in enumerate(label):
+            if a == 1:
+                if polarity[index1][count] == "positive":
+                    aspect_tf[index1][index2] = 1
+                elif polarity[index1][count] == "negative":
+                    aspect_tf[index1][index2] = 2
+                else:
+                    aspect_tf[index1][index2] = 3
+                count+=1
+    return pd.DataFrame(aspect_tf)
+
+def get_aspect_data_frame(labels):
+    df_ = create_label_dataframe(labels)
+    for aspect in aspect_labels:
+        df_[aspect]=df_[aspect].replace(1,1)
+        df_[aspect]=df_[aspect].replace(2,1)
+        df_[aspect]=df_[aspect].replace(3,1)
+    df_ = df_.fillna(0)
+    return df_
+
+def get_positive_data_frame(labels):
+    df_ = create_label_dataframe(labels)
+    for aspect in aspect_labels:
+        df_[aspect]=df_[aspect].replace(1,1)
+        df_[aspect]=df_[aspect].replace(2,0)
+        df_[aspect]=df_[aspect].replace(3,0)
+    df_ = df_.fillna(0)
+    return df_
+
+def get_negative_data_frame(labels):
+    df_ = create_label_dataframe(labels)
+    for aspect in aspect_labels:
+        df_[aspect]=df_[aspect].replace(1,0)
+        df_[aspect]=df_[aspect].replace(2,1)
+        df_[aspect]=df_[aspect].replace(3,0)
+    df_ = df_.fillna(0)
+    return df_
+
+def get_neutral_data_frame(labels):
+    df_ = create_label_dataframe(labels)
+    for aspect in aspect_labels:
+        df_[aspect]=df_[aspect].replace(1,0)
+        df_[aspect]=df_[aspect].replace(2,0)
+        df_[aspect]=df_[aspect].replace(3,1)
+    df_ = df_.fillna(0)
+    return df_
--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,68 +1,8 @@
-import emoji
-import re
-import numpy as np
-from sklearn.pipeline import make_pipeline
-from pyvi import ViTokenizer, ViPosTagger
+import preprocess_review as pre_revieew
+import preprocess_label as pre_label
 
 
-def lower_text(texts):
-    texts_result = texts.copy()
-    for i in range(len(texts_result)):
-        texts_result[i]=texts_result[i].lower()
-    return texts_result
-
-def delete_emoji(texts):
-    texts_result = texts.copy()
-    texts_result =  np.array([emoji.get_emoji_regexp().sub('', text) for text in texts_result])
-    return texts_result
-
-def replace_symbol(texts):
-  texts_result = []
-  for text in texts:
-    distance_pattern = "([0-9.,]{1,9}?.km)|([0-9.,]{1,9}?.cây số)|([0-9.,]{1,9}?.cây)|([0-9.,]{1,9}?.mét)|([0-9.,]{1,3}?.m)"
-    new_text = re.sub(distance_pattern, 'khoang_cach', text)
-    money_pattern = "(\d{1,3}k.{0})|([0-9.]{1,9}?.vnd)|([0-9.]{1,9}?.việt nam đồng)|([0-9.]{1,9}?.đồng)"
-    new_text = re.sub(money_pattern, 'gia_tien', new_text)
-    texts_result.append(new_text)
-  return texts_result
-
-def delete_special_character(texts):
-  texts_result = []
-  for text in texts:
-    special_character_pattern = "[+=<>@#$%^&~]"
-    new_text = re.sub(special_character_pattern, '', text)
-    words = new_text.split()
-    new_text = ' '.join(words)
-    texts_result.append(new_text)
-  return texts_result
-
-def normalize_elongate_words(texts):
-  texts_result = []
-  for text in texts:
-    elongate_pattern = r"(\w)\1*"
-    new_text = re.sub(elongate_pattern, r'\1', text)
-    texts_result.append(new_text)
-  return texts_result
-
-
-def replace_negative_words(texts):
-  texts_result = []
-  for text in texts:
-    hotel_pattern = r"\bksạn\b|\bk sạn\b|\bks\b|\bKS\b|\bKs\b"
-    new = re.sub(hotel_pattern, 'khách sạn', text)
-    new = re.sub(r"\bnc\b", 'nước', new)
-    new = re.sub(r"\bnvs\b|\bnhà vs\b", 'nhà vệ sinh', new)
-    new = re.sub(r"\bnv\b", 'nhân viên', new)
-    new = re.sub(r"\bvs\b", 'vệ sinh', new)
-    negative_pattern = r"\bkh\b|\bko\b|\bkhg\b|\bkhong\b|\bk\b|\bhông\b|\bhem\b|\bk0\b"
-    new = re.sub(negative_pattern, 'không', new)
-    new = re.sub(r"\bdc\b|\bdk\b", 'được', new)
-    new = re.sub(r" 1 ", " một ", new)
-    texts_result.append(new)
-  return texts_result
-
-
-class Preprocessing_dataset(object):
+class Preprocessing_Review(object):
     def __init__(self,texts ="",
                       lower_text=True, 
                       delete_emoji =True,
@@ -80,25 +20,41 @@ class Preprocessing_dataset(object):
 
     def process(self):
         result = self.texts
-        
         if self.lower_text:
-          result = lower_text(result)
-
+          result = pre_revieew.lower_text(result)
         if self.delete_emoji:
-          result = delete_emoji(result)
-
+          result = pre_revieew.delete_emoji(result)
         if self.replace_symbol:
-          result = replace_symbol(result)
-
+          result = pre_revieew.replace_symbol(result)
         if self.delete_special_character:
-          result = delete_special_character(result)
-
+          result = pre_revieew.delete_special_character(result)
         if self.replace_negative_words:
-          result = replace_negative_words(result)
-
+          result = pre_revieew.replace_negative_words(result)
         if self.normalize_elongate_words:
-          result = normalize_elongate_words(result)
-
+          result = pre_revieew.normalize_elongate_words(result)
         return result
 
 
+
+class Preprocessing_Label(object):
+    def __init__(self, label = "",
+                      aspect = False,
+                      positive = False,
+                      negative = False,
+                      neutral = False):
+      self.label = label
+      self.aspect = aspect
+      self.positive = positive
+      self.negative = negative
+      self.neutral = neutral
+    
+    def make_label_dataframe(self):
+      if self.aspect:
+        df = pre_label.get_aspect_data_frame(self.label)
+      elif self.positive:
+        df = pre_label.get_positive_data_frame(self.label)
+      elif self.negative:
+        df = pre_label.get_negative_data_frame(self.label)
+      else:
+        df = pre_label.get_neutral_data_frame(self.label)
+      return df
